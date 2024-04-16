b7dcad6e1a751746d25751a253679e996e0d3de3
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-16 20:39:13 +0300

    Update example_test.cpp

--- test/example/example_test.cpp
+++ test/example/example_test.cpp
@@ -1,11 +1,10 @@
-#include <gtest/gtest.h>
-#include "example.h"
+// #include <gtest/gtest.h>
+// #include "example.h"
 
-TEST(SuiteName, TestName) {
-    EXPECT_EQ(doSomething(10), 10);
-    // Other assertions: https://google.github.io/googletest/reference/assertions.html#boolean
-}
-
+// TEST(SuiteName, TestName) {
+//     EXPECT_EQ(doSomething(10), 10);
+//     // Other assertions: https://google.github.io/googletest/reference/assertions.html#boolean
+// }
 
 
     
