4a25bad2b7bd827de1587e32b683b1de0dcffe3b
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-16 20:00:52 +0300

    Update example_test.cpp

--- test/example/example_test.cpp
+++ test/example/example_test.cpp
@@ -1,48 +1,57 @@
-nume
-1
-1
-1
-1
-1
-1
-1
-1
-1
-1
-2
-3
-4
-19
-33
-1
-0
-0
+#include <gtest/gtest.h>
+#include "example.h"
+
+TEST(SuiteName, TestName) {
+    EXPECT_EQ(doSomething(10), 10);
+    // Other assertions: https://google.github.io/googletest/reference/assertions.html#boolean
+}
 
 
-nume
-2
-1
-1
-0
-1
-2
-3
-0
-0
-2
-0
-1
-0
-0
-1
-adresa
-23745
-1
-1
-1
-1
-4
-1
+// nume
+// 1
+// 1
+// 1
+// 1
+// 1
+// 1
+// 1
+// 1
+// 1
+// 1
+// 2
+// 3
+// 4
+// 19
+// 33
+// 1
+// 0
+// 0
+
+
+// nume
+// 2
+// 1
+// 1
+// 0
+// 1
+// 2
+// 3
+// 0
+// 0
+// 2
+// 0
+// 1
+// 0
+// 0
+// 1
+// adresa
+// 23745
+// 1
+// 1
+// 1
+// 1
+// 4
+// 1
 
 
     
