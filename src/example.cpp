4cddb29972c894e7c9acb0b7e595933579577244
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-16 20:01:18 +0300

    Update example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -1,5 +1,54 @@
-#include "example.h"
+// #include "example.h"
 
-int doSomething(int x) {
-    return x;
-}
+// int doSomething(int x) {
+//     return x;
+// }
+
+nume
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+2
+3
+4
+19
+33
+1
+0
+0
+
+
+nume
+2
+1
+1
+0
+1
+2
+3
+0
+0
+2
+0
+1
+0
+0
+1
+adresa
+23745
+1
+1
+1
+1
+4
+1
+
+
+    
