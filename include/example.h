5c411c78250c1dce286c9f8c7b2f5ed76abebb22
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-16 20:24:08 +0300

    Update example.h

--- include/example.h
+++ include/example.h
@@ -1,53 +1,5 @@
-// #ifndef OOP_EXAMPLE_H
-// #define OOP_EXAMPLE_H
-// int doSomething(int x);
-// #endif //OOP_EXAMPLE_H
+#ifndef OOP_EXAMPLE_H
+#define OOP_EXAMPLE_H
+int doSomething(int x);
+#endif //OOP_EXAMPLE_H
 
-nume
-1
-1
-1
-1
-1
-1
-1
-1
-1
-1
-2
-3
-4
-19
-33
-1
-0
-0
-
-
-nume
-2
-1
-1
-0
-1
-2
-3
-0
-0
-2
-0
-1
-0
-0
-1
-adresa
-23745
-1
-1
-1
-1
-4
-1
-
-
-    
