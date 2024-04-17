ef21c9e982807f02d549d877c5e8ce7bb73736c5
tteeeoooo <147204778+tteeeoooo@users.noreply.github.com>
2024-04-17 18:32:12 +0300

    Update example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -1,5 +1,37 @@
-#include "example.h"
+// #include "example.h"
 
-int doSomething(int x) {
-    return x;
-}
+// int doSomething(int x) {
+//     return x;
+// }
+nume
+1
+2
+0
+0
+0
+0
+
+
+nume
+2
+parola
+1
+1
+1
+1
+1
+1
+1
+2
+0
+0
+1
+adresa
+747674
+1
+354
+678
+1
+3
+1
+
